e9b796bd7758a3e5c1f9f0c36f63609406061d91
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-10 14:14:18 -0400

    variables

---
+++ src/com/fabrizio/Main.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge = myAge;
+
+        System.out.println(herAge);
+    }
+}
