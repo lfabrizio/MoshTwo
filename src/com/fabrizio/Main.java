932e58c238c55220bc82b799053ca3cc153da45f
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-11 18:15:02 -0400

    arrays

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -1,15 +1,19 @@
 package com.fabrizio;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-     // string literals
-        String message = "hello world";
-        message.endsWith("!!");
-        System.out.println(message);
+     // arrays
+        int[] numbers = new int [5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        System.out.println(Arrays.toString(numbers));
+
 
     }
 }
