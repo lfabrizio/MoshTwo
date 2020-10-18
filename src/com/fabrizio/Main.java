fcaeb147b7aab5fdf46cb3a491accb1df22816ca
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-18 18:03:36 -0400

    arithmitic expressions

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -7,8 +7,9 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-     // constants
-        final float pi = 3.14f;
+            int x = 1;
+            x += 2;
+        System.out.println(x);
 
 
     }
