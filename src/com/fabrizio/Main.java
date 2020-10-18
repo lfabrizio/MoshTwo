3eab3da3a9c1cf7f63b69f9901c764a7c6898e85
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-18 17:52:56 -0400

    constants

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -7,11 +7,8 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-     // arrays
-        int[]numbers = new int[5];
-        numbers[0] = 1;
-
-        System.out.println(numbers);
+     // constants
+        final float pi = 3.14f;
 
 
     }
