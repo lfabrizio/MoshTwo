ca04f3a473f3e00857c286c00d38a9d122a8143b
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-18 18:33:41 -0400

    the math class

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -7,11 +7,9 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-            // Implicit casting
-        // byte -> short -> int -> long -> float -> double
-        String x = "1";
-        int y = Integer.parseInt(x) + 2;
-        System.out.println(y);
+        // the math class
+        double result = Math.round(Math.random() * 100);
+        System.out.println(result);
 
 
     }
