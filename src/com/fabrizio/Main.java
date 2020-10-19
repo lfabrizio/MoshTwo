b84463bd9530fbc60bc34a9e882433c391727719
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-19 19:38:59 -0400

    logical operators

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -9,9 +9,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // comparasion operators
-    int x = 1;
-    int y = 1;
-        System.out.println(x == y);
+        // logical operators
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean isEligable = hasHighIncome || hasGoodCredit;
+        System.out.println();
     }
 }
