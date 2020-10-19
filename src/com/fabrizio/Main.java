568edc0d6dc1b36a78e72ada5fd5ae64753927ff
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-19 17:15:28 -0400

    formatting numbers

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -1,14 +1,16 @@
 package com.fabrizio;
 
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        // the math class
-        double result = Math.round(Math.random() * 100);
+        // formatting numbers 0.1 or 10%
+
+       String result =   NumberFormat.getPercentInstance().format(0.1);
         System.out.println(result);
 
 
