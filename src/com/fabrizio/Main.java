3797ebbf457042c702a348ad332322d9c3b655cd
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-19 20:00:23 -0400

    if statement

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -9,10 +9,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // logical operators
-        boolean hasHighIncome = true;
-        boolean hasGoodCredit = true;
-        boolean isEligable = hasHighIncome || hasGoodCredit;
-        System.out.println();
+        // if statements
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("Hot day");
+            System.out.println("drink water");
+        } else if (temp > 20 && temp <= 30) {
+            System.out.println("Nice day");
+        } else {
+            System.out.println("too cold");
+        }
     }
 }
