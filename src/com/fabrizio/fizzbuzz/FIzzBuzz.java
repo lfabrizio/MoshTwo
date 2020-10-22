efdb5deef14a44e9593b7b6dc8d2fd842a2e12be
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-22 17:45:41 -0400

    fizzBuzz

--- src/com/fabrizio/fizzbuzz/FIzzBuzz.java
+++ src/com/fabrizio/fizzbuzz/FIzzBuzz.java
@@ -1,5 +1,21 @@
 package com.fabrizio.fizzbuzz;
 
+import java.util.Scanner;
+
 public class FIzzBuzz {
-    // fizz buzz challenge 
+    // fizz buzz challenge
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
+            System.out.println("fizz");
+        else if (number % 3 == 0)
+            System.out.println("buzz");
+        else
+            System.out.println(number);
+    }
 }
