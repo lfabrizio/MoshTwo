0c92cd248f49474e89d0b9a2566be79810f34d24
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-22 19:18:24 -0400

    do while

--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -9,15 +9,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // while loop
-
+        // Do While Loop
         String input = "";
         Scanner scanner = new Scanner(System.in);
-        while (!input.equals("quit")){
+        do {
             System.out.println("Input: ");
             input = scanner.next().toLowerCase();
             System.out.println(input);
-        }
+        } while(!input.equals("quit"));
+        
         }
     }
 
