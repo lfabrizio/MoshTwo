b5e96a8ddff177f7aacccc6b0f61bb32c3f87337
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-27 20:23:51 -0400

    getters and setters

--- src/com/fabrizio/Employee.java
+++ src/com/fabrizio/Employee.java
@@ -1,11 +1,29 @@
 package com.fabrizio;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
 
     public int calculateWage(int extraHours){
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary){
+        if(baseSalary <= 0)
+            throw new IllegalArgumentException("error");
+            this.baseSalary = baseSalary;
+    }
+
+        public int getBaseSalary(){
+        return baseSalary;
+        }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        this.hourlyRate = hourlyRate;
     }
 }
--- src/com/fabrizio/Main.java
+++ src/com/fabrizio/Main.java
@@ -10,13 +10,14 @@ public class Main {
 
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+
+        employee.setHourlyRate(50_000);
+        employee.setBaseSalary(20);
        int wage = employee.calculateWage(10);
 
         System.out.println(wage);
 
     }
-    
+
 }
 
