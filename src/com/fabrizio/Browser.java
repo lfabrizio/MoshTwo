b479e84fc8e53e114de9d91879ee80a2b610e9cc
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-10-28 20:01:12 -0400

    reducing coupling

--- src/com/fabrizio/Browser.java
+++ src/com/fabrizio/Browser.java
@@ -2,6 +2,18 @@ package com.fabrizio;
 
 public class Browser {
     public void navigate(String address){
-        
+        String ip = findIpAddress(address);
+        String html = sendHttpRequest(ip);
+        System.out.println(html);
     }
+
+    private String sendHttpRequest(String ip) {
+        return "<html></html>";
+    }
+
+    public String findIpAddress(String address) {
+        return "127.001";
+    }
+
+
 }
